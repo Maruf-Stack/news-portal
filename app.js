bd29a275bd95370e7c2ffab874bf0d3f04587977
Maruf <01780527702maruf@gmail.com>
2022-09-03 11:16:24 +0600

    title added

--- app.js
+++ app.js
@@ -11,21 +11,21 @@ const setNavbar = async () => {
     const catagoryContainer = document.getElementById('catagory');
 
     catagories.forEach(catagory => {
-        const nav = document.createElement('navbar');
-        nav.innerHTML = `
+        const div = document.createElement('div');
+        div.classList.add('collapse');
+        div.classList.add('navbar-collapse');
+        div.innerHTML = `  
         <ul class="navbar-nav">
-        
         <li class="nav-item">
           <a class="nav-link fs-5 fw-semibold" href="#" onclick="loadCatagoriesData(${catagory.category_id})">${catagory.category_name}</a>
         </li> 
-      </ul>
+        </ul>
         `;
-        catagoryContainer.appendChild(nav);
+        catagoryContainer.appendChild(div);
     })
 }
 // show news on click 
 const loadCatagoriesData = (code) => {
-
     const url = `https://openapi.programming-hero.com/api/news/category/0${code}`;
     fetch(url)
         .then(res => res.json())
@@ -44,26 +44,28 @@ const catagoriesDetails = categorys => {
         div.classList.add("align-items-center");
         div.classList.add("justify-content-center");
         div.innerHTML = `
-    <div class="col-md-4 col-sm-12 h-100">
-    <img src="${category.image_url}" class="img-fluid w-100 rounded-start" alt="...">
+    <div class="col-md-4 col-12 h-100">
+    <img src="${category.image_url}" class="img-fluid  rounded-start main-image" alt="...">
     </div>
-    <div class="col-md-8 col-sm-12 p-5">
-        <h2>${category.title}</h2>
+    <div class="col-md-8 col-12 p-5">
+        <h2 class="text-dark fw-semibold">${category.title}</h2>
         <p class="mb-2">${category.details}</P>
         <div class="d-flex justify-content-between align-items-center">
         <div>
         <img src="${category.author.img}" class="img-fluid author rounded-circle">
         <span class="author-name">${category.author.name}</span></div>
-        <span class="view ">${category.rating.total_view}</span>
-        <button class="btn btn-primary px-3 py-2 fs-6 fw-semibold">Show details</button>
+        <span class="view fw-semibold fs-5"><i class="fa-regular fa-eye fs-6 fw-bold"></i> ${category.total_view}</span>
+        <button type="button" class="btn btn-primary px-4 py-2 border-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Show details
+    </button>
         </div>
     </div>
     `;
         catagoriesDetails.appendChild(div)
     })
-
-
-
+}
+// modal 
+const modalShow = () => {
 
 }
 setNavbar()
