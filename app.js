4c89939febfe091200489c7c5695cf1167e43e00
Maruf <01780527702maruf@gmail.com>
2022-09-03 16:12:43 +0600

    modal repeat off

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ const catagoriesDetails = categorys => {
     </div>
     <div class="col-md-8 col-12 p-5">
         <h2 class="text-dark fw-semibold">${category.title}</h2>
-        <p class="mb-2">${category.details}</P>
+        <p class="mb-2">${category.details.length > 200 ? category.details.slice(0, 200) + ('...') : category.details}</P>
         <div class="d-flex justify-content-between align-items-center">
         <div>
         <img src="${category.author.img}" class="img-fluid author rounded-circle">
@@ -74,28 +74,37 @@ const modalData = modalId => {
 
 const modalShow = modals => {
     const modalConainer = document.getElementById('modal');
+    modalConainer.textContent = '';
     modals.forEach(modal => {
-        console.log(modal)
         const div = document.createElement('div');
-        div.classList.add('modal-content')
+
+        div.classList.add('modal-content');
         div.innerHTML = `
         <div class="modal-header">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
     </div>
     <img src="${modal.image_url}">
     <div class="modal-body">
-        <h4>${modal.title}</h4>
-        <div>
-        <img src="${modal.author.img}" class="modal-author">
-        <span>${modal.author.name}</span>
+        <h4 class="mb-5"> ${modal.title}</h4>
+        <div class="mb-4">
+        <h5 class="fs-5 fw-bold">Author :</h5>
+        <img src="${modal.author.img === null ? 'author photo not found' : modal.author.img}" class="modal-author rounded-circle mt-1">
+        <span class="fw-bold fs-5 text-dark" id="author">${modal.author.name === null ? 'author name not found' : modal.author.name}</span>
+        
         </div>
+        <span class="view fw-semibold fs-5"><i class="fa-regular fa-eye fs-6 fw-bold"></i> ${modal.total_view === 0 ? 'no view yet' : modal.total_view}</span>
     </div>
     <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
         <button type="button" class="btn btn-primary">Save changes</button>
     `;
+
         modalConainer.appendChild(div);
+
+
     })
+
 }
+
 setNavbar()
 loadData()
